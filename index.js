dbe74ca8b017365724378b56c6eea5112fd416e7
mayrars <mayra203@gmail.com>
2024-10-24 07:05:56 -0600

    Call to register function

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js'
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
 app.post('/login', (req, res) => {})
-app.post('/register', (req, res) => {})
+app.post('/register', (req, res) => {
+  const { username, password } = req.body
+  try {
+    const id = UserRepository.create({ username, password })
+    res.send({ id })
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
 app.post('/logout', (req, res) => {})
 
 app.get('/protected', (req, res) => {})
