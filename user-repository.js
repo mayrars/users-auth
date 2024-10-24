03999068bd62f8c163b0f52508e11ec67c2131e9
mayrars <mayra203@gmail.com>
2024-10-24 07:05:18 -0600

    Generated method for created user and import libraries bcrypto and crypto

--- user-repository.js
+++ user-repository.js
@@ -1,6 +1,41 @@
+import crypto from 'node:crypto'
+
 import DBLocal from 'db-local'
+import bcrypt from 'bcrypt'
+
+import { SALT_ROUNDS } from './config.js'
+
+const { Schema } = new DBLocal({ path: './db' })
+
+const User = Schema('User', {
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+})
 
 export class UserRepository {
-  static create ({ username, password }) {}
+  static create ({ username, password }) {
+    if (typeof username !== 'string') throw new Error('username must be a string')
+    if (username.length < 3) throw new Error('username must be at least 3 characters long')
+
+    if (typeof password !== 'string') throw new Error('password must be a string')
+    if (password.length < 6) throw new Error('password must be at least 6 characters long')
+
+    const user = User.findOne({ username })
+    if (user) throw new Error('username already exists')
+
+    const id = crypto.randomUUID()
+    const hashPassword = bcrypt.hashSync(password, SALT_ROUNDS)
+
+    User.create({
+      _id: id,
+      username,
+      password: hashPassword
+    }).save()
+
+    return id
+  }
+
   static login ({ username, password }) {}
 }
+
