7c36b48481eb5e6d1dd5a9106c81ba9bc816e151
mayrars <mayra203@gmail.com>
2024-10-25 07:05:23 -0600

    Call to login methos and chande to async await the regsiter method

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ import { PORT } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
+app.set('view engine', 'ejs')
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('example', { username: 'May' })
 })
 
-app.post('/login', (req, res) => {})
-app.post('/register', (req, res) => {
+app.post('/login', async (req, res) => {
   const { username, password } = req.body
   try {
-    const id = UserRepository.create({ username, password })
+    const user = await UserRepository.login({ username, password })
+    res.send({ user })
+  } catch (e) {
+    res.status(401).send(e.message)
+  }
+})
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const id = await UserRepository.create({ username, password })
     res.send({ id })
   } catch (e) {
     res.status(400).send(e.message)
