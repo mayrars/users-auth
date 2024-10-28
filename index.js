cdf8e1ba581ad4b74b7a12afbe5bc9b0a7fc90cc
mayrars <mayra203@gmail.com>
2024-10-28 06:16:52 -0600

    Used cooki parser and jwt, create cookie and jwt

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import { PORT, SECRET_JWT_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -14,7 +17,17 @@ app.post('/login', async (req, res) => {
   const { username, password } = req.body
   try {
     const user = await UserRepository.login({ username, password })
-    res.send({ user })
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
+      expiresIn: '1h'
+    })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60 // 1 hour
+      })
+      .send({ user })
   } catch (e) {
     res.status(401).send(e.message)
   }
@@ -31,6 +44,7 @@ app.post('/register', async (req, res) => {
 app.post('/logout', (req, res) => {})
 
 app.get('/protected', (req, res) => {
+  const token = req.cookies.access_token
   res.render('protected')
 })
 
