7f5484b12cd4db4800fb1069038cd7c4ff7d5c4d
mayrars <mayra203@gmail.com>
2024-10-29 07:07:05 -0600

    Protected route and logout session

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    req.session.user = data
+  } catch {}
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.render('index')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -20,6 +32,11 @@ app.post('/login', async (req, res) => {
     const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
       expiresIn: '1h'
     })
+    /*
+    const refreshToken = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
+      expiresIn: '7d'
+    })
+    */
     res
       .cookie('access_token', token, {
         httpOnly: true,
@@ -41,11 +58,16 @@ app.post('/register', async (req, res) => {
     res.status(400).send(e.message)
   }
 })
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res
+    .clearCookie('access_token')
+    .json({ message: 'Logout successful' })
+})
 
 app.get('/protected', (req, res) => {
-  const token = req.cookies.access_token
-  res.render('protected')
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access not authorized')
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
